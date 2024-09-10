bc952cecfac96af3e4d0e71d75713367b84910f1
Ethan Yang <ethan.yang@intel.com>
2024-09-10 11:08:11 +0800

    Update run_lab.bat

--- run_lab.bat
+++ run_lab.bat
@@ -1,5 +1,5 @@
 @echo off
-call "C:\Users\%USERNAME%\miniconda3\Scripts\activate.bat" C:\Users\%USERNAME%\miniconda3\envs\openvino_env
+call "C:\Users\%USERNAME%\miniconda3\Scripts\activate.bat" C:\Users\%USERNAME%\miniconda3\envs\openvino_devcon
 jupyter lab .
 pause
 call "C:\Users\%USERNAME%\miniconda3\Scripts\deactivate.bat"
